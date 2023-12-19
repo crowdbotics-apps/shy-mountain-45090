26b73eed1f1f7bf400ce462e39759b90e8f820ab
Crowdbotics <team@crowdbotics.com>
2023-12-19 01:39:38 +0000

    Initial commit

---
+++ backend/shy_mountain_45090/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for shy_mountain_45090 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "shy_mountain_45090.settings"
+)
+
+application = get_wsgi_application()
